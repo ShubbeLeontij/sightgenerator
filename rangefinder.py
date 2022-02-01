aff53d6d3f7c9579759d7eccc55bf2745009916d
Leontij <60102219+Leontij@users.noreply.github.com>
2022-02-01 08:02:23 +0300

    Initial commit

---
+++ rangefinder.py
@@ -0,0 +1,57 @@
+# 3000m good zoom on RIGHT side
+
+right_line = 'line { line:p4 = 18.1, 0, 2.5, 0; thousandth:b = true; move:b=no; }\n' \
+             'line { line:p4 = 2.5, 3.1, 3.8, 3.1; thousandth:b = true; }\n' \
+             'line { line:p4 = 3.8, 2.6, 5.1, 2.6; thousandth:b = true; }\n' \
+             'line { line:p4 = 5.1, 2.1, 6.4, 2.1; thousandth:b = true; }\n' \
+             'line { line:p4 = 6.4, 1.9, 7.7, 1.9; thousandth:b = true; }\n' \
+             'line { line:p4 = 7.7, 1.6, 9.0, 1.6; thousandth:b = true; }\n' \
+             'line { line:p4 = 9.0, 1.4, 10.3, 1.4; thousandth:b = true; }\n' \
+             'line { line:p4 = 10.3, 1.2, 11.6, 1.2; thousandth:b = true; }\n' \
+             'line { line:p4 = 11.6, 1.05, 12.9, 1.05; thousandth:b = true; }\n' \
+             'line { line:p4 = 12.9, 0.95, 14.2, 0.95; thousandth:b = true; }\n' \
+             'line { line:p4 = 14.2, 0.89, 15.5, 0.89; thousandth:b = true; }\n' \
+             'line { line:p4 = 15.5, 0.82, 16.8, 0.82; thousandth:b = true; }\n' \
+             'line { line:p4 = 16.8, 0.73, 18.1, 0.73; thousandth:b = true; }\n'
+
+right_text = 'text {\ntext:t = "8"\nalign:i = 0\npos:p2 = 3.20, 3.70\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+             'text {\ntext:t = "10"\nalign:i = 0\npos:p2 = 4.45, 3.20\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+             'text {\ntext:t = "12"\nalign:i = 0\npos:p2 = 5.80, 2.70\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+             'text {\ntext:t = "14"\nalign:i = 0\npos:p2 = 7.05, 2.50\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+             'text {\ntext:t = "16"\nalign:i = 0\npos:p2 = 8.35, 2.20\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+             'text {\ntext:t = "18"\nalign:i = 0\npos:p2 = 9.65, 2.01\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+             'text {\ntext:t = "20"\nalign:i = 0\npos:p2 = 10.95, 1.81\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+             'text {\ntext:t = "22"\nalign:i = 0\npos:p2 = 12.25, 1.70\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+             'text {\ntext:t = "24"\nalign:i = 0\npos:p2 = 13.55, 1.61\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+             'text {\ntext:t = "26"\nalign:i = 0\npos:p2 = 14.85, 1.51\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+             'text {\ntext:t = "28"\nalign:i = 0\npos:p2 = 16.15, 1.46\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+             'text {\ntext:t = "30"\nalign:i = 0\npos:p2 = 17.45, 1.41\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n'
+
+# 3000m good zoom on LEFT side
+
+left_line = 'line { line:p4 = -18.1, 0, -2.5, 0; thousandth:b = true; move:b=no; }\n' \
+            'line { line:p4 = -2.5, 3.1, -3.8, 3.1; thousandth:b = true; }\n' \
+            'line { line:p4 = -3.8, 2.6, -5.1, 2.6; thousandth:b = true; }\n' \
+            'line { line:p4 = -5.1, 2.1, -6.4, 2.1; thousandth:b = true; }\n' \
+            'line { line:p4 = -6.4, 1.9, -7.7, 1.9; thousandth:b = true; }\n' \
+            'line { line:p4 = -7.7, 1.6, -9.0, 1.6; thousandth:b = true; }\n' \
+            'line { line:p4 = -9.0, 1.4, -10.3, 1.4; thousandth:b = true; }\n' \
+            'line { line:p4 = -10.3, 1.2, -11.6, 1.2; thousandth:b = true; }\n' \
+            'line { line:p4 = -11.6, 1.05, -12.9, 1.05; thousandth:b = true; }\n' \
+            'line { line:p4 = -12.9, 0.95, -14.2, 0.95; thousandth:b = true; }\n' \
+            'line { line:p4 = -14.2, 0.89, -15.5, 0.89; thousandth:b = true; }\n' \
+            'line { line:p4 = -15.5, 0.82, -16.8, 0.82; thousandth:b = true; }\n' \
+            'line { line:p4 = -16.8, 0.73, -18.1, 0.73; thousandth:b = true; }\n'
+
+left_text = 'text {\ntext:t = "8"\nalign:i = 0\npos:p2 = -3.20, 3.70\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+            'text {\ntext:t = "10"\nalign:i = 0\npos:p2 = -4.45, 3.20\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+            'text {\ntext:t = "12"\nalign:i = 0\npos:p2 = -5.70, 2.70\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+            'text {\ntext:t = "14"\nalign:i = 0\npos:p2 = -7.05, 2.50\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+            'text {\ntext:t = "16"\nalign:i = 0\npos:p2 = -8.35, 2.20\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+            'text {\ntext:t = "18"\nalign:i = 0\npos:p2 = -9.65, 2.01\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+            'text {\ntext:t = "20"\nalign:i = 0\npos:p2 = -11.05, 1.81\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+            'text {\ntext:t = "22"\nalign:i = 0\npos:p2 = -12.35, 1.70\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+            'text {\ntext:t = "24"\nalign:i = 0\npos:p2 = -13.55, 1.61\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+            'text {\ntext:t = "26"\nalign:i = 0\npos:p2 = -14.85, 1.51\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+            'text {\ntext:t = "28"\nalign:i = 0\npos:p2 = -16.15, 1.46\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n' \
+            'text {\ntext:t = "30"\nalign:i = 0\npos:p2 = -17.45, 1.41\nmove:b = no\nthousandth:b = yes\nsize:r = 0.5\nhighlight:b=yes\n}\n'
