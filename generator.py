be407636ec38fa3adee73c33517f6575bc01ae0c
Leontij <60102219+Leontij@users.noreply.github.com>
2022-02-02 06:10:53 +0300

    added settings

--- generator.py
+++ generator.py
@@ -1,5 +1,6 @@
 import os
 import rangefinder
+import json
 
 __author__ = "Shubbe Leontij"
 __license__ = "GPL"
@@ -8,44 +9,9 @@ __email__ = "leontij03@yandex.ru"
 
 
 def create_sight(foldername, filename, speed, zoom, sight_type, coord):
-    K = 5600  # DON'T TOUCH!!!
-    COLOR = '255, 255, 0, 255'  # r, g, b, tr
-    SIZE = zoom / 5  # Should be 1 if zoom == 5
-    left = True if coord[0] > 0 else False
-
-    default_circles_list = [20, 35, 50, 100, 200, 400, 600]
-    if sight_type == 0:  # With Laser Rangefinder
-        large_circles_list = []
-        small_circles_list = []
-        large_dist_list = []
-        small_dist_list = []
-    elif sight_type == 1:  # Circles AP
-        large_circles_list = [1000, 1400, 1800, 2200, 2600, 3000]
-        small_circles_list = [800, 1200, 1600, 2000, 2400, 2800]
-        large_dist_list = []
-        small_dist_list = []
-    elif sight_type == 2:  # Circles APDS
-        large_circles_list = [1400, 2200, 3000]
-        small_circles_list = [1000, 1800, 2600]
-        large_dist_list = []
-        small_dist_list = []
-    elif sight_type == 3:  # Distances AP
-        large_circles_list = []
-        small_circles_list = []
-        large_dist_list = [1000, 1400, 1800, 2200, 2600, 3000]
-        small_dist_list = [800, 1200, 1600, 2000, 2400, 2800]
-    elif sight_type == 4:  # Distances APDS
-        large_circles_list = []
-        small_circles_list = []
-        large_dist_list = [1400, 2200, 3000]
-        small_dist_list = [1000, 1800, 2600]
-    else:
-        raise Exception
-    all_dist_list = sorted(default_circles_list + large_circles_list + small_circles_list + large_dist_list + small_dist_list)
-
     def point(distance, base=50):
         parallax_x, parallax_y = coord[0] * (1 / distance - 1 / 1000) / (1 / base - 1 / 1000), coord[1] * (1 / distance - 1 / 1000) / (1 / base - 1 / 1000)
-        gravity = K * (distance - base) / speed ** 2
+        gravity = 5600 * (distance - base) / speed ** 2
         return str(round(parallax_x, 2)) + ', ' + str(round(parallax_y + gravity, 2))
 
     def crosshair_distance(distance, size):
@@ -64,11 +30,34 @@ def create_sight(foldername, filename, speed, zoom, sight_type, coord):
         return 'text\n{\ntext: t = "' + (str(distance) if distance < 100 else str(distance//100)) + '"\nalign: i = 0\npos: p2 = ' + \
                str(round(x, 2)) + ', ' + str(round(y, 2)) + '\nmove: b = yes\nthousandth: b = yes\nsize: r = ' + str(size) + '\nhighlight: b = yes\n}\n'
 
+    # Loading settings from json
+    left = True if coord[0] > 0 else False
+    settings = json.load(open('settings.json', 'r'))
+    crosshairColor = settings["crosshairColor"]
+    crosshairLightColor = settings["crosshairLightColor"]
+    rangefinderProgressBarColor1 = settings["rangefinderProgressBarColor1"]
+    rangefinderProgressBarColor2 = settings["rangefinderProgressBarColor2"]
+    fontSizeMult = settings["fontSizeMult"] * zoom / 5
+    lineSizeMult = settings["lineSizeMult"]
+
+    default_circles_list = settings["default_circles_list"]
+    for s_type in settings["sightTypes"]:
+        if s_type["type"] == sight_type:
+            large_circles_list = s_type["large_circles_list"]
+            small_circles_list = s_type["small_circles_list"]
+            large_dist_list = s_type["large_dist_list"]
+            small_dist_list = s_type["small_dist_list"]
+    all_dist_list = sorted(default_circles_list + large_circles_list + small_circles_list + large_dist_list + small_dist_list)
+
     # Start settings
-    with open('start.blk') as f:
-        output = f.read() + COLOR + '\n'
-        index = output.find('\nlineSizeMult:r=1.0')
-        output = output[:index] + str(round(SIZE, 2)) + output[index:]
+    with open('start.blk', 'r') as f:
+        output = f.read()
+        output = output.replace('$crosshairColor$', crosshairColor)
+        output = output.replace('$crosshairLightColor$', crosshairLightColor)
+        output = output.replace('$rangefinderProgressBarColor1$', rangefinderProgressBarColor1)
+        output = output.replace('$rangefinderProgressBarColor2$', rangefinderProgressBarColor2)
+        output = output.replace('$fontSizeMult$', str(fontSizeMult))
+        output = output.replace('$lineSizeMult$', str(lineSizeMult))
 
     # Distances
     output += '\ncrosshair_distances {\n'
@@ -99,9 +88,15 @@ def create_sight(foldername, filename, speed, zoom, sight_type, coord):
 
     # Text
     output += 'drawTexts{\n'
-    output += text(20, [0, 1], 0.6) + text(35, [0, 1], 0.6) + text(50, [0, 1], 0.6) + \
-              text(100, [0, -1], 0.6) + text(200, [0, -1], 0.6) + \
-              text(400, [-0.5 if left else 0.5, -0.8], 0.6) + text(600, [-0.8 if left else 0.8, -0.5], 0.6)
+    for dist in default_circles_list:
+        if dist < 100:
+            output += text(dist, [0, 1], 0.6)
+        elif dist < 300:
+            output += text(dist, [0, -1], 0.6)
+        elif dist < 500:
+            output += text(dist, [-0.5 if left else 0.5, -0.8], 0.6)
+        else:
+            output += text(dist, [-0.8 if left else 0.8, -0.5], 0.6)
     for dist in large_circles_list:
         output += text(dist, [-1.5 if left else 1.5, 0], 0.6)
     output += rangefinder.left_text if left else rangefinder.right_text
